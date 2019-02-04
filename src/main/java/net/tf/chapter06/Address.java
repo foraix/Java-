05c9b914f837186298fc3c46f2cdc9cffe5357a2
YT <362591904@qq.com>
2019-02-04 17:41:26 +0800

    有几天没有写java了，试一试

---
+++ src/main/java/net/tf/chapter06/Address.java
@@ -0,0 +1,10 @@
+package net.tf.chapter06;
+
+/**
+ * @author yuan
+ * @version 1.00
+ * @time 2019/1/19 9:04
+ * @desc
+ */
+public class Address {
+}
---
+++ src/main/java/net/tf/chapter06/StringClassTest.java
@@ -0,0 +1,10 @@
+package net.tf.chapter06;
+
+/**
+ * @author yuan
+ * @version 1.00
+ * @time 2019/1/17 17:52
+ * @desc
+ */
+public class StringClassTest {
+}
